8740cecc309d83640c31e9e9368ed5e2179ca0a6
hozerpffp <hozerpffp2009@gmail.com>
2020-05-06 22:05:04 -0500

    updating homework, more indepth and modified

--- script.js
+++ script.js
@@ -1,101 +1,118 @@
-//start button with event listner
-var startBtn = document.querySelector("#start");
-startBtn.addEventListener("click", function () {
-   
-    Question();
-    start_timer();
-   
-});
-function Question() {
-    var name = prompt("Type in first and last name : ")
- 
-} 
 
-function start_timer() {
-    var timer = document.getElementById("myTimer").innerHTML;
-    var arr = timer.split(":");
-    var hour = arr[0];
-    var min = arr[1];
-    var sec = arr[2];
-    if (sec == 0) {
-        if (min == 0) {
-            if (hour == 0) {
-                alert("Times Up");
-                window.location.reload();
+const answerContainer = document.querySelector(".answer-track");
+const options=document.querySelector(".options").children;
+const questionNumberSpan = document.querySelector(".question-num-value");
+const totalQuestionSpan = document.querySelector(".total-question");
+const question = document.querySelector(".question");
+const op1 = document.querySelector(".option1"); 
+const op2 = document.querySelector(".option2"); 
+const op3 = document.querySelector(".option3"); 
+const op4 = document.querySelector(".option4"); 
+let questionIndex;
+let index=0;
+let myArray=[];
 
-            }
-            hour--;
-            min = 60;
-            if (hour < 10) hour = "0" + hour;
+// questions and options with answer
+const questions=[
+    {
+        q: "Which of the following is the correct suntax to print a page using JavaScript?",
+        options: ["window print", "browser print", "navigator print", "document print"],
+        answer:1
+    },
+    {
+        q: "Which built in method combines the text of two strings and returns a new string?",
+        options: ["append()", "concat()", "attach()", "None of the above"],
+        answer:2
+    },
+    {
+        q: "Which built in method reverses the order of the elements of an array?",
+        options: ["changeOrder(order)", "revers()", "sort(order)", "None of the above"], 
+        answer:2
+    },
+    {
+        q: "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+        options: ["toSource()", "valueOf()", "toString()", "None of the above"],
+        answer:1
+    },
+    {
+        q: "Which of the following funtion of String object returns the characters in a string between two indexes into the string?",
+        options: ["slice()", "split()", "substr()", "substring()"],
+        answer:4
+    }
+]
+// setting in question , options 
+totalQuestionSpan.innerHTML=questions.length
+ function load() {
+    questionNumberSpan.innerHTML=index+1;
+    question.innerHTML=questions[questionIndex].q;
+    op1.innerHTML=questions[questionIndex].options[1];
+    op2.innerHTML=questions[questionIndex].options[2];
+    op3.innerHTML=questions[questionIndex].options[3];
+    op4.innerHTML=questions[questionIndex].options[4];
+    index++;
+
+ }
+ function check(element) {
+    if (element.id == questions[questionIndex].answer) {
+        element.classList.add("correct")
+       console.log('correct');
+       updateAnswer("correct");
+    } else {
+        element.classList.add("wrong")
+        updateAnswer("wrong");
+        console.log('wrong');
+    }
+   disabledOptions()
+}
+function disabledOptions() {
+    for (let i=0; i<options.length; i++) {
+        options[i].classList.add("disabled");
+        if(options[i].id==questions[questionIndex].answer) {
+            options[i].classList.add("correct");
         }
-        min--;
-        if (min < 10) min = "0" + min;
-        sec = c = 59;
-    } else sec--;
-    if (sec < 10) sec = "0" + sec;
-    document.getElementById("myTimer").innerHTML = hour + ":" + min + ":" + sec;
-    setTimeout(start_timer, 1000);
+    }
 }
 
+function enableOptions() {
+    for (let i=0; i<options.length; i++) {
+        options[i].classList.remove("disabled", "correct", "wrong");
+    }
+}
 
+function validate() {
+    // if(!options[0].classList.contains("disables")) {
+    //     alert("Select a option!!")
+    // } else {
+        enableOptions();
+        randomQuestion();
+    
+}
 
-// } 
-// function Answers() {
-//     var answer = "window print" + "browser print";//,  "navigator print", "document print"];
-//     document.getElementById("btn1", "btn2").textContent = answer;
-// }
+function next() {
+    validate();
+}
 
+ function randomQuestion() {
+     let randomNumber = Math.floor(Math.random()* questions.length);
+     questionIndex = randomNumber;
+     myArray.push(questionIndex);
+     console.log("myArray" + myArray)
+     load();
+ }
 
+ function answerTrack() {
+    for (let i=0; i<questions.length; i++) {
+        const div = document.createElement("div")
+        answerContainer.appendChild(div);
+    }
+ }
 
-// function Question(text, choices, answer) {
-//     this.text = text;
-//     this.choices = choices;
-//     this.answer = answer;
+ function updateAnswer (className) {
+     answerContainer.children[index-1].classList.add(className);
+ }
 
+     window.onload = function() {
+     randomQuestion();
+     answerTrack();
 
-//     Question.prototype.correctAnswer = function (choice) {
-//         return choice === this.answer;
-//     }
-
-
-//     this.score = 0;
-//     this.quistions = questions;
-//     this.questionIndex = 0;
-
-
-//     Question.prototype.getquestionIndex = function () {
-//         return this.questions[this.questionIndex];
-//     }
-
-//     Question.prototype.isEnded = function () {
-//         return this.questions.length === this.questionIndex;
-//     }
-
-//     Question.prototype.guess = function (answer) {
-//         this.questionIndex++;
-//         if (this.questionIndex().correctAnswer(answer)) {
-//             this.score++;
-//         }
-//     }
-
-//         if (quiz.isEnded()) {
-//             //showScores();
-//         } else {
-//             // show question
-//             var element = document.getElementById("question");
-//             element.innerHTML = quiz.getquestionIndex().text;
-
-//             //show choices
-//             var choices = quiz.getquestionIndex().choice;
-//         }
-       
-        // var questions = [
-        //      new Question("Which of the following is the correct suntax to print a page using JavaScript?", ["window print", "browser print", "navigator print", "document print"], "window print"),
-//             new Question("Which built in method combines the text of two strings and returns a new string?" ["append()", "concat()", "attach()", "None of the above"], "concat()"),
-//             new Question("Which built in method reverses the order of the elements of an array?" ["changeOrder(order)", "revers()", "sort(order)", "None of the above"], "reverse()"),
-//             new Question("Which of the following function of Boolean object returns a string containing the source of the Boolean object?" ["toSource()", "valueOf()", "toString()", "None of the above"], "toSource()"),
-//             new Question("Which of the following funtion of String object returns the characters in a string between two indexes into the string?" ["slice()", "split()", "substr()", "substring()"], "substring()"),
-//         ];
-//         var quiz = new Question(questions);
-//  }
-
+ }
